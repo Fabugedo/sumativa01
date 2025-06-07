0958c54eec73bf4adfbba85bd6b0cde6941436d9
Fabrizio Bugedo <fa.bugedo@duocuc.cl>
2025-06-07 18:11:27 -0400

    mejorando login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  standalone: false
+  standalone: false,
 })
 export class LoginPage {
+  usuario: string = '';
+  contrasena: string = '';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
+  iniciarSesion() {
+    if (!this.usuario || !this.contrasena) {
+      alert('Por favor completa ambos campos');
+      return;
+    }
 
+    if (this.usuario.length < 3 || this.usuario.length > 8) {
+      alert('El usuario debe tener entre 3 y 8 caracteres');
+      return;
+    }
 
+    const regexSoloNumeros = /^\d{4}$/;
+    if (!regexSoloNumeros.test(this.contrasena)) {
+      alert('La contraseña debe tener exactamente 4 dígitos numéricos');
+      return;
+    }
+
+    console.log('usuario ->', this.usuario);
+    console.log('contraseña ->', this.contrasena);
+
+    this.router.navigate(['/galeria']);
+  }
 }
